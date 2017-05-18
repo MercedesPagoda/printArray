00b5a609ce3cfe047b1871bf87e7c2509132c31d
MercedesPagoda <280slpagoda@gmail.com>
2017-05-19 07:14:29 +0900

    Update printIntArray.f90

--- printIntArray.f90
+++ printIntArray.f90
@@ -4,45 +4,45 @@ subroutine printIntArray(Output, IntValue, NX, NY, NZ, NMAX)
 !*  Produce the neat formatted output for an integer array                      *
 !*                                                                              *
 !********************************************************************************
-    implicit none
-    integer,intent(in)::    Output,     NX,     NY,     NZ,     NMAX
-    integer,intent(in)::    IntValue( NMAX )
-    integer::	            NXY,    x,      y,      z,      yz,                 &
-                            x1m,    x2m,    ipr,    npr,    ig,     ib,     value
-    integer,parameter::     NxPrintDefault = 15
-    real(8)::	            time
-    logical::	            qremark   
+	implicit none
+	integer,intent(in)::    Output,     NX,     NY,     NZ,     NMAX
+	integer,intent(in)::    IntValue( NMAX )
+	integer::	            NXY,    x,      y,      z,      yz,                 &
+	                        x1m,    x2m,    ipr,    npr,    ig,     ib,     value
+	integer,parameter::     NxPrintDefault = 15
+	real(8)::	            time
+	logical::	            qremark
 !================================================================================
 !	Output		:	Output stream
 !	IntValue    :	Array to be printed (stored in natural order)
 !	QNULL		:	true if grid is null 
 !================================================================================
 !--------------------------------------------------------------------------------
-!   Write heading
+!	Write heading
 !--------------------------------------------------------------------------------
-    write(Output, '("        **********************************************************************")')
-    write(Output, '("        *                        Print out Integer Array                     *")')
-    write(Output, '("        **********************************************************************")') 
+	write(Output, '("        **********************************************************************")')
+	write(Output, '("        *                        Print out Integer Array                     *")')
+	write(Output, '("        **********************************************************************")') 
 !--------------------------------------------------------------------------------
-!   NxPrintDefault grids for X axis
+!	NxPrintDefault grids for X axis
 	npr = NX / NxPrintDefault + 1
-    NXY = NX * NY
+	NXY = NX * NY
 !--------------------------------------------------------------------------------
 	do ipr=0,npr-1 
 
 		x1m	= NxPrintDefault * ipr + 1
 		x2m	= min( NX, x1m + NxPrintDefault - 1)
 
-        !Z axis
+		!Z axis
 		do z=1,NZ
 
 			qremark = (z == 1) .or. (NY > 10)
 			if (qremark) then
 				write(Output, '(\"(i,  j,  k) = ",i3,\)') x1m
-                do x=x1m+1,x2m
+				do x=x1m+1,x2m
 					write(Output, '(i8,\)') x
-                enddo
-				write(Output, *) 
+				enddo
+				write(Output, *)
 			endif
 			if (qremark .or. NY > 0) write(Output, '()') 
 
@@ -50,20 +50,20 @@ subroutine printIntArray(Output, IntValue, NX, NY, NZ, NMAX)
 			do y=1,NY
 
 				yz = (z-1) * NXY + (y-1) * NX
-                write(Output, '("(*,",i3,",",i3,") ",\)') y,z  
+				write(Output, '("(*,",i3,",",i3,") ",\)') y,z 
 
 				do x=x1m,x2m
 
 					ig = yz + x
-                    value = IntValue(ig)
+					value = IntValue(ig)
 
 !					if (QNULL(ig)) then
-!                       write(Output, '("  ***** ",\)')                        
-!					else 
+!						write(Output, '("  ***** ",\)')
+!					else
 						if (abs(value) >= 1e+8) then
-                            write(Output, '("  ----- ",\)')
+							write(Output, '("  ----- ",\)')
 						else
-                            write(Output, '(i8,\)') value 
+							write(Output, '(i8,\)') value 
 						endif
 !					endif
 
